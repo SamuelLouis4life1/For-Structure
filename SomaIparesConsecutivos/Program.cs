80f28a5b1d3e3bf85b05e1224a5bf0bda5ce7cfc
Samuel Louis <sampeur4life1@gmai.com>
2020-01-13 22:15:15 -0300

    Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos números impares entre eles

---
+++ SomaIparesConsecutivos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/// <summary>
+/// Uri 1071
+/// Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos números impares entre eles
+/// </summary>
+
+namespace SomaIparesConsecutivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, soma, min, max;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                min = x;
+                max = y;
+            }
+            else
+            {
+                min = y;
+                max = x;
+            }
+
+            soma = 0;
+            for (int i = min + 1; i < max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
